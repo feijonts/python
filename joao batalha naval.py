1c7a152a0061ee0e6dfe09b9efb25d8f83956a7e
joaocitino10 <joaocitino10@gmail.com>
2016-06-06 17:36:42 -0300

    batalha naval 06 de junho

---
+++ joao batalha naval.py
@@ -0,0 +1,36 @@
+from random import randint
+
+print "vamos jogar batalha naval"
+
+jogo = True
+
+tabuleiro = []
+linhas =5
+colunas=5
+
+linha = ["O"] * colunas
+
+for x in range(colunas):
+    tabuleiro.append(["0"] * colunas)
+
+navio_linha = randint(4,linhas-1)
+navio_coluna = randint(4,colunas-1)
+
+def mostra_tabuleiro(t):
+    for L in t:
+        print " ".join(L)
+        
+while jogo:
+    mostra_tabuleiro(tabuleiro)
+    chute_linha = input("qual linha?")-1
+    chute_coluna = input("qual coluna?")-1
+
+    if chute_linha == navio_linha and chute_coluna == navio_coluna:
+        print "Parabéns! Você acertou!!"
+        jogo = False
+    elif chute_linha >= linhas or chute_coluna >= colunas:
+        print "Você chutou fora do tabuleiro"
+    elif tabuleiro[chute_linha][chute_coluna] == "x":
+        print "você já chutou aí"
+    else:
+        tabuleiro[chute_linha][chute_coluna] = "x"
